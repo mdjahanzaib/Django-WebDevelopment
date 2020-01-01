96245cb7c03f5a9db0dacdc02cd2e70fc9ef341d
mdjahanzaib <m_jahanzaib@outlook.com>
2020-01-01 20:48:31 +0500

    setting theme

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-  return render(request,'home.html',{'name':'Myname'})
+  return render(request,'home.html',{'name':'Jahanzaib'})
+def contact(request):
+  return render(request,'cont.html',{'cont':'Contacts'})
